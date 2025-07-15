54ea084d05ca7613dbe74fa25ba6150c605e5419
Rejhine <rrejhine@gmail.com>
2025-07-15 22:17:45 +0800

    Modify tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,21 @@
 import { icons } from '@/constants/icons';
-import { images } from '@/constants/images';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Image, ImageBackground, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-const TabIcon = ({ focused, icon, title }) => {
-    if(focused) {
-        return (
-            <>
-                <ImageBackground
-                    source={images.highlight}
-                    className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
-                >
-                    <Image 
-                        source={icon}
-                        tintColor='#151312'
-                        className='size-5' 
-                    />
-                    <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
-                </ImageBackground>
-            </>
-        )
-    }
-    
+const TabIcon = ({ focused, icon, title }: { focused: boolean; icon: any; title: string }) => {
     return (
         <>
-            <View className='size-full justify-center items-center mt-4 rounded-full'>
+            <View
+                className='flex-1 mt-3 flex flex-col items-center'
+            >
                 <Image 
                     source={icon}
-                    tintColor="#a8b5db"
-                    className='size-5'
+                    tintColor={focused ? '#B09FCA' : '#666876'}
+                    resizeMode='contain'
+                    className='size-6' 
                 />
+                <Text className={`${focused ? 'text-accent' : 'text-primary'} text-xs w-full text-center mt-1`}>{title}</Text>
             </View>
         </>
     )
@@ -41,22 +26,12 @@ const _layout = () => {
     <Tabs
         screenOptions={{
             tabBarShowLabel: false,
-            tabBarItemStyle: {
-                width: '100%',
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center'
-            },
             tabBarStyle: {
-                backgroundColor: '#0f0d23',
-                borderRadius: 50,
-                marginHorizontal: 20,
-                marginBottom: 36,
-                height: 52,
+                backgroundColor: 'white',
                 position: 'absolute',
-                overflow: 'hidden',
-                borderWidth: 1,
-                borderColor: '#0f0d23'
+                borderTopColor: '#0061ff1a',
+                borderTopWidth: 1,
+                minHeight: 70,
             }
         }}
     >
@@ -82,7 +57,7 @@ const _layout = () => {
                 tabBarIcon: ({ focused }) => (
                     <TabIcon 
                         focused={focused} 
-                        icon={icons.search}
+                        icon={icons.explore}
                         title="Explore" 
                     />
                 )
@@ -96,7 +71,7 @@ const _layout = () => {
                 tabBarIcon: ({ focused }) => (
                     <TabIcon 
                         focused={focused} 
-                        icon={icons.save}
+                        icon={icons.library}
                         title="Library" 
                     />
                 )
@@ -110,7 +85,7 @@ const _layout = () => {
                 tabBarIcon: ({ focused }) => (
                     <TabIcon 
                         focused={focused} 
-                        icon={icons.person}
+                        icon={icons.profile}
                         title="Profile" 
                     />
                 )
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,11 +1,15 @@
-import home from "@/assets/icons/home.png";
-import search from "@/assets/icons/search.png";
-import person from "@/assets/icons/person.png";
-import logo from "@/assets/icons/logo.png";
-import save from "@/assets/icons/save.png";
-import star from "@/assets/icons/star.png";
-import play from "@/assets/icons/play.png";
+import add from "@/assets/icons/add.png";
 import arrow from "@/assets/icons/arrow.png";
+import explore from "@/assets/icons/explore.png";
+import home from "@/assets/icons/home.png";
+import library from "@/assets/icons/library.png";
+import logo from "@/assets/icons/logo.png";
+import person from "@/assets/icons/person.png";
+import play from "@/assets/icons/play.png";
+import profile from "@/assets/icons/profile.png";
+import save from "@/assets/icons/save.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
 
 export const icons = {
   home,
@@ -16,4 +20,8 @@ export const icons = {
   star,
   play,
   arrow,
+  explore,
+  add,
+  library,
+  profile
 };
